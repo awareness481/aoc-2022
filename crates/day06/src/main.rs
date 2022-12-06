f5799c1c6696fc424c661a541ca9ae7ae02d54a0
Jesse Jafa <jesse.jafa@gmail.com>
2022-12-06 16:50:45 +0200

    improve day 06 answer

--- crates/day06/src/main.rs
+++ crates/day06/src/main.rs
@@ -3,44 +3,25 @@ use std::collections::HashSet;
 fn main() {
     const INPUT: &str = include_str!("./input.txt");
     // let binding = INPUT.split('\n').collect::<Vec<&str>>();
-    println!("part 1: {}", detect_first_unique(INPUT));
-    println!("part 2: {}", detect_first_unique_2(INPUT));
+    println!("part 1: {}", detect_first_unique(INPUT, 4));
+    println!("part 2: {}", detect_first_unique(INPUT, 14));
 }
 
-fn detect_first_unique(input: &str) -> usize {
-    let inp = input.chars().collect::<Vec<char>>();
+fn detect_first_unique(input: &str, size: usize) -> usize {
+    let binding = input.chars().into_iter().collect::<Vec<char>>();
+    let windows = binding.windows(size);
+
     let mut final_index = 0;
-    for (i, c) in inp.iter().enumerate() {
+
+    for (i, window) in windows.enumerate() {
         let mut set = HashSet::new();
-        set.insert(c);
-        set.insert(&inp[i + 1]);
-        set.insert(&inp[i + 2]);
-        set.insert(&inp[i + 3]);
 
-        if set.len() == 4 {
-            println!("{:?} {}", &set, i + 4);
-            final_index = i + 4;
-            break;
-        }
-    }
-
-    final_index
-}
-
-fn detect_first_unique_2(input: &str) -> usize {
-    let inp = input.chars().collect::<Vec<char>>();
-    let mut final_index = 0;
-    for (i, c) in inp.iter().enumerate() {
-        let mut set = HashSet::new();
-        set.insert(c);
-
-        for j in 1..14 {
-            set.insert(&inp[i + j]);
+        for c in window {
+            set.insert(c);
         }
 
-        if set.len() == 14 {
-            println!("{:?} {}", &set, i + 14);
-            final_index = i + 14;
+        if set.len() == size {
+            final_index = i + size;
             break;
         }
     }
@@ -54,23 +35,32 @@ mod tests {
 
     #[test]
     fn test_detect_first_unique() {
-        assert_eq!(detect_first_unique("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(detect_first_unique("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-        assert_eq!(detect_first_unique("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(detect_first_unique("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(detect_first_unique("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(detect_first_unique("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(
+            detect_first_unique("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4),
+            10
+        );
+        assert_eq!(
+            detect_first_unique("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4),
+            11
+        );
     }
 
     #[test]
     fn test_detect_first_unique_2() {
-        assert_eq!(detect_first_unique_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
-        assert_eq!(detect_first_unique_2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
-        assert_eq!(detect_first_unique_2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
         assert_eq!(
-            detect_first_unique_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            detect_first_unique("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14),
+            19
+        );
+        assert_eq!(detect_first_unique("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(detect_first_unique("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(
+            detect_first_unique("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
             29
         );
         assert_eq!(
-            detect_first_unique_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            detect_first_unique("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14),
             26
         );
     }
